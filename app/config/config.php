a66fcd1b010352669e316d84066b01c284382f22
david <davidwidjaya97@gmail.com>
2022-06-10 19:26:12 +0700

    Initial Commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+    chdir(dirname(__DIR__));
+    // Config Loader
+    $f3->instance()->config("app/config/config.ini");   
+
+    $f3->set('DB', new \DB\SQL('mysql:host=127.0.0.1;port=3306;dbname=skripsirey;charset=utf8',
+        'root',
+        '', [
+            \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            ])
+    );
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'david/skripsi',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => '3.8.0',
+            'version' => '3.8.0.0',
+            'reference' => 'cdd878db018280369b643fce62f70e55044f9ac4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree-core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'david/skripsi' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
